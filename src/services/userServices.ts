4789be12e12739481a47bf7cf51f59703c03f4aa
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2024-01-27 17:32:21 -0300

    feat: hash password before store on db

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,8 +1,11 @@
 import { User } from "@prisma/client";
+import { hashSync } from "bcrypt";
 import { userRepository } from "../repositories/userRepositories";
 
 async function signUp(user: User) {
-  await userRepository.create(user);
+  const salts = process.env.HASH_SALTS || 12
+  const password = hashSync(user.password, salts);
+  await userRepository.create({...user, password});
 }
 
 export const userService = {
