64b00ff46f37f4b5ad54ae97c47e2d3563d5c1d4
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2024-01-28 18:27:16 -0300

    refactor: change public routers structure

--- src/routers/publicRouters/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signUp } from "../../controllers";
-
-const publicRouters = Router();
-
-publicRouters.use('/user', signUp);
-
-export default publicRouters;
