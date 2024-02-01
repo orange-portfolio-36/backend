7e3034f35782cd09975b7f585c6c8356e8c6d1f6
Pedro <pedrocastillo24.pc@gmail.com>
2024-01-31 23:35:02 -0300

    fix; conflict wih app and errorMiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import "express-async-errors";
 import cors from "cors";
 import helmet from "helmet";
@@ -7,14 +7,11 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 import {serve as serveSwagger, setup} from "swagger-ui-express";
 import swaggerDocument from "./docs/swagger";
 
-
-
 const app = express();
 
+app.use(json());
 app.use(cors());
 app.use(helmet());
-
-app.use(express.json());
 
 app.use('/', router);
 
--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,15 +1,12 @@
 
-import {Request, Response, NextFunction } from "express";
+import {NextFunction, Request, Response } from "express";
 import { CustomError } from "../@types";
 
 export function errorMiddleware(err: CustomError, req: Request, res: Response, next: NextFunction){
- 
+    console.log(err.stack)
 
     const statusCode = err.statusCode || 500;
     const errorMessage = err.message || 'Internal Server Error';
-
-    
   
-    res.status(statusCode).json({ error: errorMessage });
-
+    res.status(statusCode).send({ error: errorMessage });
 }
