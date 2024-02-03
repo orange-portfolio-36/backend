73660ff1d95818359cac2bc81021477ada793b3c
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2024-02-03 09:55:15 -0300

    feat: add signout route

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers";
+import { signIn, signOut, signUp } from "../controllers";
 import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware";
 import { createUserSchema, signInSchema } from "../schemas";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const userRouter = Router();
 
 userRouter
   .post("/signup", validateSchemaMiddleware(createUserSchema), signUp)
-  .post("/signin", validateSchemaMiddleware(signInSchema), signIn);
+  .post("/signin", validateSchemaMiddleware(signInSchema), signIn)
+  .post("/signout", authMiddleware, signOut);
 
 export default userRouter;
